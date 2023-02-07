6f6d1f069c27e16136fd7010ab36fd769a62aac1
fahmida <fahmida.nusrat135@gmail.com>
2023-02-08 01:28:21 +0600

    th commit

--- basic.py
+++ basic.py
@@ -23,4 +23,25 @@ print(s1[1:2])
 # escaping
 # \ "
 # \ '
-#
+# \\
+# \n
+
+# formated string
+first = "first"
+last = "last"
+full = first + " " + last
+print(full)
+
+first = "first"
+last = "last"
+full1 = f"{first}{2+2}"
+print(full1)
+
+# string method
+course = "python programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print("programming" in course)
+print(course.find("pro"))
+print(course.replace("pr", "_"))
